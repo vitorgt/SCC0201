056adbb336ab5cb617be5eb46dd78712aa837066
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2017-11-27 20:42:58 -0200

    parser

--- t5_fluxo_de_caixa_para_terminal_de_auto_atendimento.c
+++ t5_fluxo_de_caixa_para_terminal_de_auto_atendimento.c
@@ -2,11 +2,9 @@
 #include<stdlib.h>
 #include<string.h>
 
-#define hashR 5
-
 typedef struct _ll{
 	int t, b, d;//terminal, banco, banco de destino
-	char o;//operacao
+	char o, e;//operacao, erro
 	float v;//valor
 	struct _ll *n;//proxima celula
 }ll;
@@ -16,66 +14,72 @@ void clearll(ll *a){
 	a->b = 0;
 	a->d = 0;
 	a->o = 0;
+	a->e = 0;
 	a->v = 0;
 	a->n = NULL;
 }
 
-int hashmap(int k){
-	return k;
+int hashmap(char o, int t){
+	return 4*t-4+((o == 'S') ? 0 : ((o == 'D') ? 1 : ((o == 'C') ? 2 : ((o == 'T') ? 3 : -1))));
 }
 
 int main(){
-	int read = 0;
-	char c = 0;
+	int i = 0;
+	char c = 0, buffer[100] = {0};
 	float f = 0;
 	ll reader;
 	while(!feof(stdin)){
 		clearll(&reader);
 		read = 0; c = 0; f = 0;
-		if((c = getchar()) == 'E'){
-			reader.o = c;
-			//error
-					while(c != '\n'){
-						c = getchar();
-					}
-		}
-		else{
+		scanf("%d ", &(reader.t));
+		c = getchar();
+		if(c >= '0' && c <= '9'){
 			ungetc(c, stdin);
-			scanf("%d ", &(reader.t));
-			c = getchar();
-			if(c >= '0' && c <= '9'){
-				ungetc(c, stdin);
-				scanf("%d ", &read);
-				reader.b = read;
-				reader.o = getchar();
-				if(reader.o == 'C'){
-					while(c != '\n'){
-						c = getchar();
-					}
+			scanf("%d ", &(reader.b));
+			reader.o = getchar();
+			if(reader.o == 'C'){
+				while(c != '\n'){
+					c = getchar();
+				}
+			}
+			else{
+				scanf("%f", &f);
+				if((c = getchar()) == ' '){
+					reader.d = (int)f;
+					scanf("%f\n", &reader.v);
 				}
 				else{
-					scanf("%f", &f);
-					if((c = getchar()) == ' '){
-						reader.d = (int)f;
-						scanf("%f\n", &reader.v);
+					if(reader.o == 'T')
+						reader.d = reader.b;
+					while(c == '\r'){
+						c = getchar();
 					}
-					else{
-						if(reader.o == 'T')
-							reader.d = reader.b;
-						while(c == '\r'){
-							c = getchar();
-						}
-						reader.v = f;
-					}
+					reader.v = f;
 				}
 			}
-			else if(c == 'S' || c == 'D' || c == 'C' || c == 'T'){
-				reader.o = c;//ler teminal e char == AUDITORIA
-				scanf(" %d\n", &reader.d);
-				//hashmap
-			}
 		}
-		printf("%d\t%3d\t%c\t%d\t%8.2f\t%p\n", reader.t, reader.b, reader.o, reader.d, reader.v, (void*)reader.n);
+		else if(c == 'S' || c == 'D' || c == 'C' || c == 'T'){
+			reader.o = c;//ler teminal e char == AUDITORIA
+			scanf(" %d\n", &reader.d);
+			//hashmap
+		}
+		for(i = 0; (c = getchar()); i++){
+			buffer[i] = c;
+			if(c == 'E'){
+				reader.e = 1;
+				while(1){
+					c = getchar();
+					if(c == 0 || c == '\n' || c == EOF)
+						break;
+				}
+			}
+			if(c == 0 || c == '\n' || c == EOF)
+				break;
+		}
+		if(!reader.e)
+			for(; i >= 0; i--)
+				ungetc(buffer[i], stdin);
+		printf("%d\t%3d\t%c\t%d\t%8.2f\t%d\t%p | %d\n", reader.t, reader.b, reader.o, reader.d, reader.v, (int)reader.e, (void*)reader.n, hashmap(reader.o, reader.t));
 	}
 	return 0;
 }
